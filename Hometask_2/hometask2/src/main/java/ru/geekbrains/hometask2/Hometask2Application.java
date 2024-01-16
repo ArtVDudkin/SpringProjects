1b1cb641c460a7cc8c9ea38725ff9460b4504843
Artem <kardan97@yandex.ru>
2024-01-16 19:51:51 +0700

    Initial commit. Readme file created

---
+++ Hometask_2/hometask2/src/main/java/ru/geekbrains/hometask2/Hometask2Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hometask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hometask2Application.class, args);
+	}
+
+}
